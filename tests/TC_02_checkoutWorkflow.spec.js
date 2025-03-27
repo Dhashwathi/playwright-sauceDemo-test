fff0323e4485db84b0f4f60ece2ea3526d8d885d
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-03-27 16:29:24 +0530

    ADD-TC_02_checkoutWorkflow test file is added

---
+++ tests/TC_02_checkoutWorkflow.spec.js
@@ -0,0 +1,25 @@
+import {test,expect} from '@playwright/test'
+
+test("verify products page and logout", async({page})=>{
+    await page.goto("https://www.saucedemo.com/");
+    await page.locator("#user-name").fill("standard_user");
+    await page.locator("#password").fill("secret_sauce");
+    await page.click("#login-button");
+    await page.click("#add-to-cart-sauce-labs-backpack");
+    await page.click("#add-to-cart-sauce-labs-bolt-t-shirt");
+    await page.click("#shopping_cart_container");
+    await page.click("#checkout");
+    await page.locator("#first-name").fill("Dhash");
+    await page.locator("#last-name").fill("MB");
+    await page.locator("#postal-code").fill("637205");
+    await page.click("#continue");
+    await page.click("#finish");
+    //assert
+    expect(page.locator("#back-to-products")).toContainText("Back Home");
+    await page.click("#back-to-products");
+    await page.click("#react-burger-menu-btn");
+    await page.click("#logout_sidebar_link");
+    //assert
+    expect(page.locator("#login-button")).toBeVisible();
+
+})
